edd264b3cfd818001321a03f70c7af3f77097bd8
Muratyaman80 <mrtymn1980@gmail.com>
2024-06-02 02:50:41 +0300

    v01

---
+++ src/test/java/turkcell_pasaj/Runners/Runner.java
@@ -0,0 +1,23 @@
+package turkcell_pasaj.Runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:src/test/resources/htmlReporter/Rapor.html",
+                "json:src/test/resources/htmlReporter/Rapor.json",
+                "junit:src/test/resources/htmlReporter/Rapor.xml"
+
+        },
+        monochrome = true,
+        features = "src/test/resources/ui_features",
+        glue = {"turkcell_pasaj/stepdefs", "turkcell_pasaj/hooks"},
+        tags = "",
+        dryRun = false
+)
+public class Runner {
+}
